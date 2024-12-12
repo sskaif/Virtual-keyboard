6657a9c4bb1fa8f3b0a461a79843935dafd20501
saif <saif12@gmail.com>
2024-12-12 15:17:19 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const buttons = document.querySelectorAll('.btn')
+const textarea = document.querySelector('textarea')
+
+const delete_btn = document.querySelector('.delete')
+const shift_btn = document.querySelector('.shift')
+const space_btn = document.querySelector('.space')
+
+let chars = []
+
+document.body.addEventListener('keydown',(e)=>{
+    console.log(e.key)
+    textarea.value += e.key
+    chars = textarea.value.split('')
+  
+    
+
+
+})
+
+
+buttons.forEach(btn => {
+    btn.addEventListener('click', () => {
+        textarea.value += btn.innerText
+        chars = textarea.value.split('')
+    })
+})
+
+
+delete_btn.addEventListener('click', () => {
+    chars.pop()
+    textarea.value = chars.join('')
+})
+
+space_btn.addEventListener('click', () => {
+    chars.push(' ')
+    textarea.value = chars.join('')
+})
+
+shift_btn.addEventListener('click', () => {
+    buttons.forEach(btn => {
+        btn.classList.toggle('upper')
+    })
+})
